4c934fc587b3d215e226f64949d06ccd751d6d48
eelin9763 <eelin@dmacc.edu>
2020-02-21 12:53:43 -0600

    fixed exit option to only say goodbye

--- lib/photo/cli.rb
+++ lib/photo/cli.rb
@@ -11,7 +11,7 @@ class Photo::CLI
 
     @photos = Photo::Album.now
     @photos.each.with_index(1) do |photo, i|
-      puts "(#{i}). #{photo.albumId}"
+      puts "(#{i}). #{photo.albumId} #{photo.title} #{photo.url} #{photo.thumbnailUrl}"
     end
   end
 
@@ -25,7 +25,7 @@ class Photo::CLI
         the_photo = @photos[input.to_i - 1]
         puts "#{the_photo.albumId} #{the_photo.id} #{the_photo.title} #{the_photo.url} #{the_photo.thumbnailUrl}"
         puts "Scroll above to see your photos! Then, type another number to see another album or type exit to terminate program:"
-      else
+      elsif input.to_i > 1 || input.to_i < 100
         puts "Only photo-albums between 1-100"
       end
     end
